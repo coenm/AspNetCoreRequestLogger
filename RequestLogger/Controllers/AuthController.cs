87798c1eb4a1b176102804a22c41325ac336bed2
Coen Munckhof <c.munckhof@gmail.com>
2019-12-09 23:37:00 +0100

    auth

---
+++ RequestLogger/Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RequestLogger.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Get()
+        {
+            return Ok("OK");
+        }
+    }
+}
